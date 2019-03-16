33daff3801ede92292a48dcb08c433649f221718
gedennis <gedennis@163.com>
2019-03-16 21:30:41 +0800

    feat(device): finish the bonjour device component

---
+++ devices/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'Device 1',
+  port: 3000
+};
--- devices/index.js
+++ devices/index.js
@@ -1,12 +1,15 @@
 const Koa = require('koa');
 const http = require('http');
-const bonjour = require('bonjour')();
 const IO = require('koa-socket-2');
+
+const config = require('./config');
+const Device = require('./components/device');
 
 const app = new Koa();
 const io = new IO();
 
-const NAME = process.env.DEVNAME || 'noname';
+const NAME = process.env.DEVNAME || config.name;
+const PORT = process.env.PORT || config.port;
 app.use(ctx => {
   ctx.body = `Hello, this is device ${NAME}`;
 });
@@ -18,19 +21,11 @@ io.on('message', (ctx, data) => {
 });
 
 const server = http.createServer(app.callback());
-server.listen(process.env.PORT || 3000);
+server.listen(PORT);
 server.on('listening', () => {
-  // advertise a service
-  const service = bonjour.publish({
-    name: NAME,
-    type: 'http',
-    port: server.address().port
-  });
-  service.on('up', () => {
-    console.log(`device ${NAME} is up...`);
-  });
-  service.on('error', err => {
-    console.log(`device ${NAME} publish with error...`, err);
-  });
+  const type = 'http';
+  const device = new Device(NAME, type, PORT);
+  device.init();
+
   console.info('Server is listening on port:%d', server.address().port);
 });
