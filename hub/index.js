fa76ce42932fb9419d612b68ba03428e74238474
gedennis <gedennis@163.com>
2019-03-17 11:00:34 +0800

    feat(device): add socket.io

--- hub/index.js
+++ hub/index.js
@@ -1,27 +1,36 @@
+const fs = require('fs');
 const Koa = require('koa');
-const http = require('http');
-const IO = require('koa-socket-2');
 const mdns = require('mdns');
+const http = require('http');
+const path = require('path');
+const socket = require('socket.io');
 
 const config = require('./config');
 
 const app = new Koa();
-const io = new IO();
 
 const NAME = process.env.DEVNAME || config.name;
 const PORT = process.env.PORT || config.port;
+// app.use(ctx => {
+//   ctx.body = `Hello, this is device ${NAME}`;
+// });
 app.use(ctx => {
-  ctx.body = `Hello, this is device ${NAME}`;
-});
-
-// wechat server
-io.attach(app);
-
-io.on('message', (ctx, data) => {
-  console.log('client sent data to message endpoint', data);
+  ctx.type = 'text/html';
+  ctx.body = fs.createReadStream(path.join(__dirname, 'index.html'));
 });
 
 const server = http.createServer(app.callback());
+const io = socket(server);
+io.on('connection', socket => {
+  console.log('A device connected');
+  socket.on('message', message => {
+    console.log('Receive message from device', message);
+  });
+  socket.on('disconnect', function(msg) {
+    console.log('device disconnected');
+    console.log('disconnect', msg);
+  });
+});
 server.listen(PORT);
 server.on('listening', () => {
   const type = 'http';
